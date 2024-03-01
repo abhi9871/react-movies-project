5030901782796f195eaab81df72a3ef095b24393
abhi9871 <at719273@gmail.com>
2024-03-01 14:58:52 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, {useState} from 'react';
-import MoviesList from './components/MoviesList';
-import Loader from './components/Loader';
-import './App.css';
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import Loader from "./components/Loader";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-    const fetchedMovies = data.results.map((movie) => {
-            return {
-                id: movie.episode_id,
-                title: movie.title,
-                releaseDate: movie.release_date,
-                openingText: movie.opening_crawl
-            };
-          }); 
-          setMovies(fetchedMovies); 
-          setIsLoading(false);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if(!response.ok) {
+        throw new Error('Something went wrong...Retrying');
+    }
+
+      const data = await response.json();
+      const fetchedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        };
+      });
+      
+      setMovies(fetchedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  };
+
+  let content = <p>Found no movies.</p>
+
+  if(movies.length > 0){
+    content = <MoviesList movies={movies} />;
+  }
+
+  if(error){
+    content = <p>{error}</p>;
+  }
+
+  if(isLoading){
+    content = <Loader />;
   }
 
   return (
@@ -28,7 +54,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {(isLoading) ? <Loader /> : <MoviesList movies={movies} />}
+        {/* {isLoading ? <Loader /> : <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>} */}
+        {content}
       </section>
     </React.Fragment>
   );
